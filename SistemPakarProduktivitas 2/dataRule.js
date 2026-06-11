ca816fea73495a80c9ad1b9866cc62cabe980bca
alfinfh <gakadarame@gmail.com>
2026-06-12 05:48:15 +0800

    Add files via upload

---
+++ SistemPakarProduktivitas 2/dataRule.js
@@ -0,0 +1,123 @@
+const rules = [
+
+  {
+    kode: "R1",
+    gejala: ["G01", "G02", "G03", "G04"],
+    hasil: "P1"
+  },
+
+  {
+    kode: "R2",
+    gejala: ["G01", "G03", "G05", "G06"],
+    hasil: "P1"
+  },
+
+  {
+    kode: "R3",
+    gejala: ["G01", "G02", "G03"],
+    hasil: "P2"
+  },
+
+  {
+    kode: "R4",
+    gejala: ["G02", "G05", "G07"],
+    hasil: "P2"
+  },
+
+  {
+    kode: "R5",
+    gejala: ["G03", "G04", "G08"],
+    hasil: "P2"
+  },
+
+  {
+    kode: "R6",
+    gejala: ["G01", "G03"],
+    hasil: "P3"
+  },
+
+  {
+    kode: "R7",
+    gejala: ["G02", "G05"],
+    hasil: "P3"
+  },
+
+  {
+    kode: "R8",
+    gejala: ["G11", "G13"],
+    hasil: "P4"
+  },
+
+  {
+    kode: "R9",
+    gejala: ["G12", "G14"],
+    hasil: "P4"
+  },
+
+  {
+    kode: "R10",
+    gejala: ["G15", "G16"],
+    hasil: "P4"
+  },
+
+  {
+    kode: "R11",
+    gejala: ["G11", "G12", "G13"],
+    hasil: "P5"
+  },
+
+  {
+    kode: "R12",
+    gejala: ["G14", "G15", "G18"],
+    hasil: "P5"
+  },
+
+  {
+    kode: "R13",
+    gejala: ["G16", "G17", "G19"],
+    hasil: "P5"
+  },
+
+  {
+    kode: "R14",
+    gejala: ["G11", "G20"],
+    hasil: "P4"
+  },
+
+  {
+    kode: "R15",
+    gejala: ["G18", "G19"],
+    hasil: "P5"
+  },
+
+  {
+    kode: "R16",
+    gejala: ["G21", "G22", "G23"],
+    hasil: "P1"
+  },
+
+  {
+    kode: "R17",
+    gejala: ["G22", "G24", "G25"],
+    hasil: "P2"
+  },
+
+  {
+    kode: "R18",
+    gejala: ["G21", "G25"],
+    hasil: "P3"
+  },
+
+  {
+    kode: "R19",
+    gejala: ["G26", "G27"],
+    hasil: "P4"
+  },
+
+  {
+    kode: "R20",
+    gejala: ["G28", "G29", "G30"],
+    hasil: "P5"
+  }
+
+];
